644e950719fa9a7136034b3f79749a957fa4e3b4
Bilger Yahov <bilger@studyportals.com>
2017-11-18 19:04:21 +0100

    Fix docs.

--- LumenAPI/app/Exceptions/Handler.php
+++ LumenAPI/app/Exceptions/Handler.php
@@ -28,13 +28,13 @@ class Handler extends ExceptionHandler {
         return parent::report($e);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
-     */
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Exception  $e
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
     public function render($request, Exception $e)
     {
     	if(env('APP_DEBUG')){
