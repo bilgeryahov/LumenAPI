6925e417c78fc9cf824894a6abfc7a275faec022
Bilger Yahov <bilger@studyportals.com>
2017-11-18 17:41:10 +0100

    Implement POSTing in sub-routes.

--- LumenAPI/app/Http/Controllers/CourseStudentController.php
+++ LumenAPI/app/Http/Controllers/CourseStudentController.php
@@ -3,13 +3,13 @@
 namespace  App\Http\Controllers;
 
 use App\Course;
+use App\Student;
 
 class CourseStudentController extends Controller{
 
 	public function index($course_id){
 
 		$course = Course::find($course_id);
-
 		if($course){
 
 			$students = $course->students;
@@ -20,9 +20,29 @@ class CourseStudentController extends Controller{
 		return $this->createErrorResponse("The course with id {$course_id} has not been found.", 404);
 	}
 
-	public function store(){
+	public function store($course_id, $student_id){
 
-		return __METHOD__;
+		$course = Course::find($course_id);
+		if($course){
+
+			$student = Student::find($student_id);
+			if($student){
+
+				if($course->students()->find($student->id)){
+
+					return $this->createErrorResponse("The student with id {$student_id} 
+					is already in the list of students for the course with id {$course_id}", 409);
+				}
+
+				$course->students()->attach($student->id);
+				return $this->createSuccessResponse("The students with id {$student_id} has been 
+				successfully added to the list of students for the course with id {$course_id}", 201);
+			}
+
+			return $this->createErrorResponse("The student with id {$student_id} has not been found.", 404);
+		}
+
+		return $this->createErrorResponse("The course with id {$course_id} has not been found.", 404);
 	}
 
 	public function destroy(){
--- LumenAPI/app/Http/Controllers/TeacherCourseController.php
+++ LumenAPI/app/Http/Controllers/TeacherCourseController.php
@@ -1,27 +1,52 @@
 <?php
 namespace  App\Http\Controllers;
 
+use App\Course;
 use App\Teacher;
+use Illuminate\Http\Request;
 
 class TeacherCourseController extends Controller{
+
+	private $rules = [
+		'title' => 'required',
+		'description' => 'required',
+		'value' => 'required|numeric'
+	];
 
 	public function index($teacher_id){
 
 		$teacher = Teacher::find($teacher_id);
-
 		if($teacher){
 
 			$courses = $teacher->courses;
-
 			return $this->createSuccessResponse($courses, 200);
 		}
 
 		return $this->createErrorResponse("The teacher with id {$teacher_id} has not been found.", 404);
 	}
 
-	public function store(){
+	public function store(Request $request, $teacher_id){
 
-		return __METHOD__;
+		$teacher = Teacher::find($teacher_id);
+		if($teacher){
+
+			$this->validateRequest($request);
+
+			// If we pass validation...
+			$course = Course::create(
+				[
+					'title' => $request->get('title'),
+					'description' => $request->get('description'),
+					'value' => $request->get('value'),
+					'teacher_id' => $teacher->id
+				]
+			);
+
+			return $this->createSuccessResponse("The course with id {$course->id} has been successfully 
+			created and associated with teacher with id {$teacher->id}", 201);
+		}
+
+		return $this->createErrorResponse("The teacher with id {$teacher_id} has not been found.", 404);
 	}
 
 	public function update(){
@@ -32,5 +57,10 @@ class TeacherCourseController extends Controller{
 	public function destroy(){
 
 		return __METHOD__;
+	}
+
+	private function validateRequest($request){
+
+		$this->validate($request, $this->rules);
 	}
 }
